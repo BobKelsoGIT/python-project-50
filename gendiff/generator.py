17000b6b5cb12f8f928f43ebddea4835c0018a71
Ivan Scherbakov <lolwut155@gmail.com>
2024-01-29 18:29:38 +0300

    linter fix

--- gendiff/generator.py
+++ gendiff/generator.py
@@ -18,15 +18,26 @@ def generate_diff_list(data1, data2):
         value2 = data2.get(key)
 
         if key in added_keys:
-            diff_lines.append({'name': key, 'status': 'added', 'new_value': value2})
+            diff_lines.append({
+                'name': key,
+                'status': 'added',
+                'new_value': value2})
         elif key in deleted_keys:
-            diff_lines.append({'name': key, 'status': 'deleted', 'old_value': value1})
+            diff_lines.append({
+                'name': key,
+                'status': 'deleted',
+                'old_value': value1})
         elif isinstance(value1, dict) and isinstance(value2, dict):
             diff_lines.append(nested(key, value1, value2))
         elif value1 != value2:
-            diff_lines.append({'name': key, 'status': 'changed', 'old_value': value1, 'new_value': value2})
+            diff_lines.append({'name': key,
+                               'status': 'changed',
+                               'old_value': value1,
+                               'new_value': value2})
         else:
-            diff_lines.append({'name': key, 'status': 'unchanged', 'value': value1})
+            diff_lines.append({'name': key,
+                               'status': 'unchanged',
+                               'value': value1})
 
     sorted_diff = sorted(diff_lines, key=lambda x: x['name'])
 
