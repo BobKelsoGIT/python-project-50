6ce70d98d90d0076ca37f4e3fa7d6a927920cc96
Ivan Scherbakov <lolwut155@gmail.com>
2024-02-02 19:58:18 +0300

    added test_generate_diff.py

--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -2,39 +2,14 @@ import pytest
 from gendiff.generate_diff import generate_diff
 
 
-@pytest.fixture
-def initial_data():
-    return "tests/fixtures/file1.yml"
-
-
-@pytest.fixture
-def modified_data():
-    return "tests/fixtures/file2.yml"
-
-
-@pytest.fixture
-def expected_stylish():
-    with open('tests/fixtures/expected_stylish.txt') as file:
-        return file.read()
-
-
-@pytest.fixture
-def expected_json():
-    with open('tests/fixtures/expected_json.txt') as file:
-        return file.read()
-
-
-@pytest.fixture
-def expected_plain():
-    with open('tests/fixtures/expected_plain.txt') as file:
-        return file.read()
-
-
-@pytest.mark.parametrize("expected_result, formatter", [
-    (expected_stylish, "stylish"),
-    (expected_json, "json"),
-    (expected_plain, "plain"),
+@pytest.mark.parametrize("initial_data, modified_data, expected_result_path, formatter", [
+    ("tests/fixtures/file1.yml", "tests/fixtures/file2.yml", "tests/fixtures/expected_stylish.txt", "stylish"),
+    ("tests/fixtures/file1.yml", "tests/fixtures/file2.yml", "tests/fixtures/expected_json.txt", "json"),
+    ("tests/fixtures/file1.yml", "tests/fixtures/file2.yml", "tests/fixtures/expected_plain.txt", "plain"),
 ])
-def test_generate_diff(initial_data, modified_data, expected_result, formatter):
+def test_generate_diff(initial_data, modified_data, expected_result_path, formatter):
+    with open(expected_result_path) as file:
+        expected_result = file.read()
+
     result = generate_diff(initial_data, modified_data, style=formatter)
     assert result == expected_result
