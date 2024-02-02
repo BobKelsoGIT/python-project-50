7d4dad33d655d7ee2d5bde4ddfb8702f631a3581
Ivan Scherbakov <lolwut155@gmail.com>
2024-02-02 14:21:30 +0300

    added tests for stylish

--- tests/test_stylish.py
+++ tests/test_stylish.py
@@ -2,7 +2,58 @@ from gendiff.formatter.stylish import make_stylish, to_str
 
 
 def test_make_stylish():
-    pass
+    value = [
+        {
+            'name': 'first_level',
+            'status': 'nested',
+            'children': [
+                {'name': 'second_level1', 'status': 'changed', 'old_value': False, 'new_value': True},
+                {'name': 'second_level2', 'status': 'unchanged', 'value': 'foo'},
+                {'name': 'second_level3', 'status': 'added', 'new_value': 'baz'},
+            ]
+        }
+    ]
 
-def test_to_str():
-    pass
+    result = """{
+    first_level: {
+      - second_level1: false
+      + second_level1: true
+        second_level2: foo
+      + second_level3: baz
+    }
+}"""
+
+    assert result == make_stylish(value)
+
+
+def test_to_str_true():
+    value = True
+    assert to_str(value) == 'true'
+
+
+def test_to_str_false():
+    value = False
+    assert to_str(value) == 'false'
+
+
+def test_to_str_none():
+    value = None
+    assert to_str(value) == 'null'
+
+
+def test_to_str_dict():
+    value = {
+        "host": "hexlet.io",
+        "timeout": 50,
+        "proxy": "123.234.53.22",
+        "follow": False
+    }
+
+    expected_result = '''{
+        host: hexlet.io
+        timeout: 50
+        proxy: 123.234.53.22
+        follow: false
+    }'''
+
+    assert to_str(value) == expected_result
