79cbe0b83c28b98f5dcf9d31133de497b16380e8
Ivan Scherbakov <lolwut155@gmail.com>
2024-02-02 22:49:57 +0300

    add test_parser.py

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -2,8 +2,13 @@ from gendiff.parser import get_data, get_extension
 
 
 def test_get_extension():
-    pass
+    assert get_extension('file.json') == 'json'
+    assert get_extension('file.yml') == 'yml'
+    assert get_extension('file.yaml') == 'yaml'
 
 
-def test_get_data():
-    pass
+def test_get_data_json():
+    file_path = 'tests/fixtures/file1.json'
+    result = get_data(file_path)
+    expected_result = {'host': 'hexlet.io', 'timeout': 50, 'proxy': '123.234.53.22', 'follow': False}
+    assert result == expected_result
