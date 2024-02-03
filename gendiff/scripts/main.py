11815172af4d42a3854f326d928d7581b9d96790
Ivan Scherbakov <lolwut155@gmail.com>
2024-02-04 00:28:39 +0300

    main.py refactored

--- gendiff/scripts/main.py
+++ gendiff/scripts/main.py
@@ -4,9 +4,7 @@ from gendiff.cli import parse_args
 
 def main():
     args = parse_args()
-    data1 = args.first_file
-    data2 = args.second_file
-    print(generate_diff(data1, data2, args.format))
+    print(generate_diff(args.first_file, args.second_file, args.format))
 
 
 if __name__ == '__main__':
