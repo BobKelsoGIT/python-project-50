4f24d6b8923088634c7193f8cf6d620bb5ae5338
Ivan Scherbakov <lolwut155@gmail.com>
2024-02-04 00:21:54 +0300

    generator.py dismissed changes

--- gendiff/generator.py
+++ gendiff/generator.py
@@ -1,18 +1,52 @@
+def nested(key, value1, value2) -> dict:
+    """
+    Generate children's keys with names, statuses and values
+    """
+    return {
+        'name': key,
+        'status': 'nested',
+        'children': generate_diff_list(value1, value2)
+    }
+
+
 def generate_diff_list(data1, data2) -> list:
+    """
+    Generate list of keys with names, statuses and values
+    """
+    union_keys = data1.keys() | data2.keys()
+    added_keys = data2.keys() - data1.keys()
+    deleted_keys = data1.keys() - data2.keys()
+
     diff_lines = []
 
-    for key in set(data1.keys()) | set(data2.keys()):
-        value1, value2 = data1.get(key), data2.get(key)
+    for key in union_keys:
+        value1 = data1.get(key)
+        value2 = data2.get(key)
 
-        if key in data2 and key not in data1:
-            diff_lines.append({'name': key, 'status': 'added', 'new_value': value2})
-        elif key in data1 and key not in data2:
-            diff_lines.append({'name': key, 'status': 'deleted', 'old_value': value1})
+        if key in added_keys:
+            diff_lines.append({
+                'name': key,
+                'status': 'added',
+                'new_value': value2})
+        elif key in deleted_keys:
+            diff_lines.append({
+                'name': key,
+                'status': 'deleted',
+                'old_value': value1})
         elif isinstance(value1, dict) and isinstance(value2, dict):
-            diff_lines.append({'name': key, 'status': 'nested', 'nested_diff': generate_diff_list(value1, value2)})
+            diff_lines.append(nested(key, value1, value2))
         elif value1 != value2:
-            diff_lines.append({'name': key, 'status': 'changed', 'old_value': value1, 'new_value': value2})
+            diff_lines.append({
+                'name': key,
+                'status': 'changed',
+                'old_value': value1,
+                'new_value': value2})
         else:
-            diff_lines.append({'name': key, 'status': 'unchanged', 'value': value1})
+            diff_lines.append({
+                'name': key,
+                'status': 'unchanged',
+                'value': value1})
 
-    return sorted(diff_lines, key=lambda x: x['name'])
+    sorted_diff = sorted(diff_lines, key=lambda x: x['name'])
+
+    return sorted_diff
